68efc24ec46c96a3bd05dfb75612b665cb24fdae
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-07-12 09:33:49 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -22,7 +22,7 @@ class InteractiveRecord
       column_names.compact
     end
 
-    def self.column_names.each do |col_name|
+    self.column_names.each do |col_name|
       attr_accessor col_name.to_sym
     end
 
